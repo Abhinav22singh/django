13081ad635dc779a2e14cca3dad8cf32c7a65c04
Abhinav Singh <singhabhinav2205@gmail.com>
2026-08-19 15:42:05 +0530

    contact_working

--- Home/views.py
+++ Home/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from Home.models import Contact
+from django.contrib import messages
 def index(request):
-    return render(request,"index.html")
+    context = {'variable': "This is sent"}
+    messages.success(request, "This is a test message.")
+    return render(request, "index.html", context)
 def about(request):
     return render(request,"about.html")
 def services(request):
     return HttpResponse("Services Page")
 def contact(request):
-    return render(request,"contact.html")
+    if request.method == "POST":
+        firstname = request.POST.get('first_name')
+        lastname = request.POST.get('last_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact(firstname=firstname, lastname=lastname, email=email, message=message, date=datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent successfully!")
+    return render(request, "contact.html")
 
+  
 # Create your views here.
